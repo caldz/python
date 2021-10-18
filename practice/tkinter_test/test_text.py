5e9e36363cb5aa958a3de849f504de0338ad56ed
caldz <136820607@qq.com>
2021-10-19 03:04:35 +0800

    更新文字框DEMO

--- practice/tkinter_test/test_text.py
+++ practice/tkinter_test/test_text.py
@@ -1,24 +1,32 @@
 import tkinter
 
 
+def text_key_event(event,text):
+    print('keycode={}'.format(event.keycode))
+
+def win_key_event(event,win):
+    # print('keycode={}'.format(event.keycode))
+    if (event.keycode==27):
+        win.quit()
 if __name__=='__main__':
     print('in')
     win=tkinter.Tk()
+    win.bind('<Key>',lambda event:win_key_event(event,win))
+
     text=tkinter.Text(win)
     scroll=tkinter.Scrollbar()
     
+    #关联滚动条和文字框
     scroll.config(command=text.yview)
     text.config(yscrollcommand=scroll.set)
-    help(text.config)
-    text.insert('insert','1111')
-    text.insert('end','3333')
-    text.insert('0.0','2222')
     
-    
+    #布局滚动条和文字框
     scroll.pack(side=tkinter.RIGHT,fill=tkinter.Y)
     text.pack()
+    text.bind('<Key>',lambda event:text_key_event(event,text))
+    help(text)
     
-    for i in range(50):
+    for i in range(20):
         text.insert('end','{}\n'.format(i))
         print(i)
     #把光标和屏幕移到文本末端
