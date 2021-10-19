62958e7ce07d38fbe0fa44d89b9f6b3d86bd9b2f
caldz <136820607@qq.com>
2021-10-19 14:14:07 +0800

    update

--- practice/tkinter_test/test_text.py
+++ practice/tkinter_test/test_text.py
@@ -1,35 +1,41 @@
 import tkinter
+import re
 
+def text_key_event(e,text):
+    if e.keycode==13:
+        last_line=text.get('end-1l','end')
+        line_cmd=last_line.strip()
+        if line_cmd!='':
+            print(line_cmd)
 
-def text_key_event(event,text):
-    print('keycode={}'.format(event.keycode))
+def win_key_event(e):
+    if e.keycode==27:
+        e.widget.quit()
 
-def win_key_event(event,win):
-    # print('keycode={}'.format(event.keycode))
-    if (event.keycode==27):
-        win.quit()
 if __name__=='__main__':
     print('in')
     win=tkinter.Tk()
-    win.bind('<Key>',lambda event:win_key_event(event,win))
+    win.bind('<Key>',win_key_event)
+
 
     text=tkinter.Text(win)
+    text.bind('<Key>',lambda e:text_key_event(e,text))
     scroll=tkinter.Scrollbar()
-    
+        
     #关联滚动条和文字框
     scroll.config(command=text.yview)
     text.config(yscrollcommand=scroll.set)
     
+    
     #布局滚动条和文字框
     scroll.pack(side=tkinter.RIGHT,fill=tkinter.Y)
     text.pack()
-    text.bind('<Key>',lambda event:text_key_event(event,text))
-    help(text)
     
     for i in range(20):
         text.insert('end','{}\n'.format(i))
         print(i)
     #把光标和屏幕移到文本末端
     text.see('end')
+    
     win.mainloop()
     print('exit')
