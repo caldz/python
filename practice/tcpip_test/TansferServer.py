f3f25b8984fc89f6fde37ed469690a75461ecc29
caldz <136820607@qq.com>
2021-11-24 19:23:37 +0800

    update

--- practice/tcpip_test/TansferServer.py
+++ practice/tcpip_test/TansferServer.py
@@ -3,54 +3,42 @@ import sys
 import socketserver
 import socket
 import threading
-from typing import TextIO
-import FileOp
 import time
 from threading import Event
 
 logging.basicConfig(format="%(asctime)s %(thread)d %(threadName)s %(message)s", stream=sys.stdout, level=logging.INFO)
 
-
 class Handler(socketserver.BaseRequestHandler):
-
+    
     def setup(self):
         self.event = threading.Event()
         logging.info("新加入了一个连接{}".format(self.client_address))
-        file_name = './{}_{}.txt'.format('tag', time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime()))
-        self.file = open(file_name, mode='wb')
 
     def handle(self):
-        sk = self.request
+        sk: socket.socket = self.request
         while not self.event.is_set():
             try:
                 data = sk.recv(1024)
                 if len(data) > 0:
-                    self.has_data=True
-                    logging.info('recv[{}]:{}'.format(len(data), data))
-                    self.file.write(data)
+                    print('recv[{}]:{}'.format(len(data), data))
             except Exception as e:
+                logging.info('hehe')
                 logging.info(e)
-                logging.info('断开链接111')
-                sk.close()
-                break
-
+            break
         logging.info(data)
         # msg = "{}-{}".format(self.client_address, data).encode()
         # sk.send(msg)
-        
 
     def finish(self):
-        logging.info('断开连接')
+        print('断开连接')
         self.event.set()
         self.request.close()
-        self.file.close()
 
 
 if __name__ == '__main__':
-    server_config = FileOp.get_server_config()
-    print(server_config)
-    server = socketserver.ThreadingTCPServer((server_config['server_ip'], server_config['server_port']), Handler)
+    server = socketserver.ThreadingTCPServer(('172.16.24.5',22222), Handler)
     threading.Thread(target=server.serve_forever, name="server").start()
+    
     while True:
         cmd = input(">>>")
         if cmd.strip() == "quit":
@@ -59,3 +47,5 @@ if __name__ == '__main__':
             break
         logging.info(threading.enumerate())
     print('exit')
+    
+    
